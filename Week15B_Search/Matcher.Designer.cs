8f1d7043cbefbdcd5b5bf4b9cbaafd787c05f507
ferenckovacsx <ferenckovacsx@gmail.com>
2016-05-04 11:15:13 +0200

    Initial commit

---
+++ Week15B_Search/Matcher.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Week15B_Search
+{
+    partial class Matcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPattern = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxMatched = new System.Windows.Forms.RichTextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(384, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Matched";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pattern";
+            // 
+            // textBoxPattern
+            // 
+            this.textBoxPattern.Location = new System.Drawing.Point(69, 226);
+            this.textBoxPattern.Name = "textBoxPattern";
+            this.textBoxPattern.Size = new System.Drawing.Size(289, 20);
+            this.textBoxPattern.TabIndex = 5;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(346, 179);
+            this.richTextBox.TabIndex = 8;
+            this.richTextBox.Text = "";
+            // 
+            // richTextBoxMatched
+            // 
+            this.richTextBoxMatched.Location = new System.Drawing.Point(387, 27);
+            this.richTextBoxMatched.Name = "richTextBoxMatched";
+            this.richTextBoxMatched.Size = new System.Drawing.Size(228, 179);
+            this.richTextBoxMatched.TabIndex = 9;
+            this.richTextBoxMatched.Text = "";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(387, 224);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Matcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 263);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.richTextBoxMatched);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.textBoxPattern);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Matcher";
+            this.Text = "Matcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPattern;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.RichTextBox richTextBoxMatched;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
---
+++ Week15B_Search/Matcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week15B_Search
+{
+    public partial class Matcher : Form
+    {
+        public Matcher()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            MatchCollection matches = Regex.Matches(richTextBox.Text, @textBoxPattern.Text);
+
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                {
+                    richTextBox.Select(match.Index, match.Length);
+                    richTextBox.SelectionBackColor = Color.Pink;
+                    richTextBoxMatched.Text = match.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sorry, no matches!");
+            }
+
+      
+        }
+    }
+}
